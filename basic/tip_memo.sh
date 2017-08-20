d5be5b7188e3f5899614d1eef15539208bab49f0
working user <yoko@localhost.localdomain>
2017-08-20 23:53:06 +0900

    tip追記

--- basic/tip_memo.sh
+++ basic/tip_memo.sh
@@ -2,3 +2,11 @@ cd /home/yoko/dev/key2shell/basic
 cd /home
 # 一つ前のカレントディレクトリへ移動
 cd -
+
+# 拡張子がtxt、shのファイルのみ表示
+echo "拡張子がtxt、shのみ表示"
+ls *.{txt,sh} 2> /dev/null
+
+# /etc/passwdの一列目だけ表示
+echo "/etc/passwdの一列目だけをソートして表示"
+cut -d: -f1 < /etc/passwd | sort
