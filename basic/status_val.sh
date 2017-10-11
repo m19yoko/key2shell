72c31b5fee07bb3d493d783a5bbc7cc428276375
working user <yoko@localhost.localdomain>
2017-10-11 20:35:52 +0900

    状態変数シェルを更新

--- basic/status_val.sh
+++ basic/status_val.sh
@@ -12,7 +12,29 @@ echo $BASH
 echo $BASH_VERSION
 # カレントディレクトリ
 echo $PWD
+base_path=$PWD
 cd ../
 cd ../
 # 一つ前のカレントディレクトリ
 echo $OLDPWD
+# 変数の宣言とエクスポートを同時にする
+export www2="hahaha"
+echo "環境変数 www2 を定義した ${www2}"
+# test.shでwww2を呼び出しても使用できる。
+# ただし、www2はこれを呼び出したセッションからは参照できない。
+# cd $base_path
+# ./test.sh
+
+# 標準変数いろいろ
+# ディスプレイの列数(shell上からは表示されない)
+echo $COLUMNS
+# テキストエディタのパス名
+echo $EDITOR
+# ディスプレイの行数(shell上からは表示されない)
+echo $LINES
+# シェルのパス名
+echo $SHELL
+# 使用している端末の種類
+echo $TERM
+
+
